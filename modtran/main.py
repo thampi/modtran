9e7f0f9faf81f5726337c9796ea0c2fbee2a8a17
thampi <gauravthampi7@gmail.com>
2022-02-24 18:00:57 -0500

    Update main.py

--- modtran/main.py
+++ modtran/main.py
@@ -730,7 +730,7 @@ def run(username: str,                         # CIS username
         tape5 += "\n"
     output = {}
     output['tape5'] = tape5
-    print(tape5)
+#     print(tape5)
         
     # Logging generated tape5:
     print('=======================================\n=======================================')
